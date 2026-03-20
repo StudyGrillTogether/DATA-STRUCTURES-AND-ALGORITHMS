955f43add5f03ced4a055a998e275de58657b74d
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-20 14:54:28 +0530

    added a switcch case to implement the methods

--- DAY-4/DAY-4-PRACTICE/DAY-4-PRACTICE/Program.cs
+++ DAY-4/DAY-4-PRACTICE/DAY-4-PRACTICE/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,9 @@ namespace DAY_4_PRACTICE
         }
         class LinkedList
         {
-            Node head; //points to first node
+             Node head; //points to first node
 
-            void AddFirst(int value)
+            public void AddFirst(int value)
             {
                 Node firstNode=new Node();
                 firstNode.data = value;
@@ -33,7 +34,7 @@ namespace DAY_4_PRACTICE
             /// thus completing the operation of the addfirst method
             /// </summary>
             /// <param name="args"></param>
-            void AddLast(int value)
+            public void AddLast(int value)
             {
                 Node newNode=new Node();
                 newNode.data = value;
@@ -61,12 +62,67 @@ namespace DAY_4_PRACTICE
             ///we use current as a temporary pointer to traverse the list without losing the head
             ///so we loop using the while loop and find the current last node
             ///then we upadtes it pointer to our new last node that is here new node thus completing the operation
-            ///
+            
+            public void Display()
+            {
+                if (head == null)
+                {
+                    Console.WriteLine("The list is empty:");
+                    return;
+                }
+                else
+                {
+                    Node current = head;
+                    while (current!= null)
+                    {
+                        Console.WriteLine(current.data+" -> ");
+                        current = current.next;
+                    }
+                }
+            }
         }
+        
         
         static void Main(string[] args)
         {
+            LinkedList list=new LinkedList();
+            
+            Console.WriteLine("1.ADD A VALUE at the start of LIST");
+            Console.WriteLine("2.Add node at the last of the list");
+            Console.WriteLine("3.Display the list");
+            Console.WriteLine("4.Exit");
+            
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine("Enter your choice: ");
+                string choice=Console.ReadLine();
 
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("enter the value you want to add: ");
+                        int value = int.Parse(Console.ReadLine());
+                        list.AddFirst(value);
+                        break;
+                    case "2":
+                        Console.WriteLine("enter the value you want to add: ");
+                        int value2 = int.Parse(Console.ReadLine());
+                        list.AddLast(value2);
+                        break;
+                    case "3":
+                        list.Display();
+                        break;
+                    case "4":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid INPUT!");
+                        return;
+                        break;
+
+                }
+            }
         }
     }
 }
