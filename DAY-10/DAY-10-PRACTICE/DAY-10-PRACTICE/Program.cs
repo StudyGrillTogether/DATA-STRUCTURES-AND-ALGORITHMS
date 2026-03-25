55ac309b0eb9684d115c6853359f01b8a2e27ec5
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-25 17:10:47 +0530

    Day 10 DSA: Efficient Sorting - MergeSort and QuickSort

--- DAY-10/DAY-10-PRACTICE/DAY-10-PRACTICE/Program.cs
+++ DAY-10/DAY-10-PRACTICE/DAY-10-PRACTICE/Program.cs
@@ -50,6 +50,37 @@ namespace DAY_10_PRACTICE
 
             return result;
         }
+
+        static void QuickSort(int[] array,int low,int high)
+        {
+            if (low >= high)
+            {
+                return;
+            }
+            int pivotIndex = Partition(array, low, high);
+            QuickSort(array, low, pivotIndex-1);
+            QuickSort(array, pivotIndex + 1, high);
+        }
+        static int Partition(int[] array,int low,int high)
+        {
+            int pivot = array[high];
+            int i= low - 1;
+            
+            for(int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i = i + 1;
+                    int temp1 = array[i];
+                    array[i] = array[j];
+                    array[j] = temp1;                    
+                }              
+            }
+            int temp = array[i + 1];
+            array[i + 1] = array[high];
+            array[high] = temp;
+            return i + 1;
+        }
         
         static void Main(string[] args)
         {
@@ -59,6 +90,9 @@ namespace DAY_10_PRACTICE
             {
                 Console.WriteLine(i);
             }
+            QuickSort(array, 0, array.Length - 1);
+            foreach(int n in array)
+                Console.WriteLine(n+" ");   
         }
     }
 }
