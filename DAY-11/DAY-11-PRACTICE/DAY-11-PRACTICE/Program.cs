e64d563dce9614a9d971fd2b86d6ee5a19bebfb2
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-26 11:50:27 +0530

    included methods for inorder and preorder in binary tree clas

--- DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Program.cs
+++ DAY-11/DAY-11-PRACTICE/DAY-11-PRACTICE/Program.cs
@@ -25,7 +25,27 @@ namespace DAY_11_PRACTICE
         class BinaryTree
         {
             Node root; //points to root node
+
+            public void InOrder(Node node)
+            {
+                if (node == null) return;
+                InOrder(node.left); //go left
+                Console.WriteLine(node.data); //print
+                InOrder(node.right);//go right
+            }
+
+            public void PreOrder(Node node)
+            {
+                if (node == null) return;
+                Console.WriteLine(node.data);
+                PreOrder(node.left);
+                PreOrder(node.right);
+            }
+
+            public void PostOrder(Node node) {
         }
+
+        
         static void Main(string[] args)
         {
         }
